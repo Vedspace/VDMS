463c24c1798611c11d579e8bc8b41b6303e82639
khushi <khushishukla5890@gmail.com>
2022-10-01 11:40:24 +0530

    global css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom"
+import React from "react";
+
 function App() {
   return (
-    <div className="App">
-         App
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+                <Route path="/Login" element={<Login/>} />
+                <Route path="/SignUp" element={<SignUp/>} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Login = () => {
+ 
+  return (
+    <div style = {{background:"var(--primary-color)", minHeight: "100vh"}}>
+        Login
+    </div>
+      );
+};
+
+export default Login;
