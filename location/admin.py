f27cbc2bc0cb90fc0f6059b1123cabcfc5fc0132
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-22 03:07:12 +0530

    Delete location directory

--- location/admin.py
+++
@@ -1,8 +0,0 @@
-# location/admin.py
-
-from django.contrib import admin
-from .models import UserLocation
-
-@admin.register(UserLocation)
-class UserLocationAdmin(admin.ModelAdmin):
-    list_display = ['user', 'latitude', 'longitude', 'timestamp']
