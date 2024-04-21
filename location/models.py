b64355450fe158649800cefdad2fe5aff74a7b2e
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-22 03:31:46 +0530

    fg
    das

---
+++ location/models.py
@@ -0,0 +1,15 @@
+# location/models.py
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserLocation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.latitude}, {self.longitude}"
