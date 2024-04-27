1b5fb09e976383c1e0b2b574df3785df2bbcca8e
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 01:17:47 +0530

    Update admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,32 +1,23 @@
-import pytz
 from django.contrib import admin
 from django.utils import timezone
 from .models import Attendance
 
-@admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('worker', 'date', 'format_time1', 'format_time2', 'format_time3')
 
     def format_time1(self, obj):
-        return self.localize_time(obj.time1)
+        return timezone.localtime(obj.time1).strftime('%Y-%m-%d %H:%M:%S') if obj.time1 else None
     format_time1.admin_order_field = 'time1'
     format_time1.short_description = 'Time 1'
 
     def format_time2(self, obj):
-        return self.localize_time(obj.time2)
+        return timezone.localtime(obj.time2).strftime('%Y-%m-%d %H:%M:%S') if obj.time2 else None
     format_time2.admin_order_field = 'time2'
     format_time2.short_description = 'Time 2'
 
     def format_time3(self, obj):
-        return self.localize_time(obj.time3)
+        return timezone.localtime(obj.time3).strftime('%Y-%m-%d %H:%M:%S') if obj.time3 else None
     format_time3.admin_order_field = 'time3'
     format_time3.short_description = 'Time 3'
 
-    def localize_time(self, datetime_obj):
-        """Converts naive datetime to timezone-aware datetime for display."""
-        if datetime_obj is None:
-            return None
-        if timezone.is_naive(datetime_obj):
-            # Make it timezone aware using UTC from pytz
-            datetime_obj = timezone.make_aware(datetime_obj, pytz.utc)
-        return timezone.localtime(datetime_obj, pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S')
+admin.site.register(Attendance, AttendanceAdmin)
