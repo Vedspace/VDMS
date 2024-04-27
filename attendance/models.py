bf24f2ed0a7c34f2f983215a608c984de04bf3d3
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 01:06:48 +0530

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 class Attendance(models.Model):
     worker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
