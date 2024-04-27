2dba5bcf0866a1317cb9216df06b3f33eededeaa
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 01:15:07 +0530

    Update admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,4 @@
+import pytz
 from django.contrib import admin
 from django.utils import timezone
 from .models import Attendance
@@ -8,8 +9,8 @@ class AttendanceAdmin(admin.ModelAdmin):
 
     def format_time1(self, obj):
         return self.localize_time(obj.time1)
-    format_time1.admin_order_field = 'time1'  # Allows column order sorting
-    format_time1.short_description = 'Time 1'  # Column header
+    format_time1.admin_order_field = 'time1'
+    format_time1.short_description = 'Time 1'
 
     def format_time2(self, obj):
         return self.localize_time(obj.time2)
@@ -26,6 +27,6 @@ class AttendanceAdmin(admin.ModelAdmin):
         if datetime_obj is None:
             return None
         if timezone.is_naive(datetime_obj):
-            # Assume datetime is in UTC, make it timezone aware and convert to local timezone
-            datetime_obj = timezone.make_aware(datetime_obj, timezone.ist)
-        return timezone.localtime(datetime_obj).strftime('%Y-%m-%d %H:%M:%S')
+            # Make it timezone aware using UTC from pytz
+            datetime_obj = timezone.make_aware(datetime_obj, pytz.utc)
+        return timezone.localtime(datetime_obj, pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S')
