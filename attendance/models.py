e0204e79b5cac552c372358bd269615448d36989
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 00:40:55 +0530

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.conf import settings
-from django.utils.timezone import now
 
 class Attendance(models.Model):
     worker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateField()
-    time1 = models.DateTimeField(null=True, blank=True, default=now)
-    time2 = models.DateTimeField(null=True, blank=True, default=now)
-    time3 = models.DateTimeField(null=True, blank=True, default=now)
-
-
+    time1 = models.TimeField(null=True, blank=True)
+    time2 = models.TimeField(null=True, blank=True)
+    time3 = models.TimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.worker.phone} - {self.date}"
+        return f"{self.worker.username} - {self.date}"
