e204be6a69a17326383b217c090795d8b0415d78
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-27 16:39:54 +0530

    Update settings.py

--- Hometasks/settings.py
+++ Hometasks/settings.py
@@ -49,9 +49,7 @@ INSTALLED_APPS = [
     
     
 ]
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static"),
- ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
