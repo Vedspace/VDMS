7c5152e8028f664c55965d70128b38e3648a5bf5
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 01:19:22 +0530

    Update admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,22 +2,35 @@ from django.contrib import admin
 from django.utils import timezone
 from .models import Attendance
 
+@admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('worker', 'date', 'format_time1', 'format_time2', 'format_time3')
 
     def format_time1(self, obj):
-        return timezone.localtime(obj.time1).strftime('%Y-%m-%d %H:%M:%S') if obj.time1 else None
+        return self.format_time(obj.time1)
     format_time1.admin_order_field = 'time1'
     format_time1.short_description = 'Time 1'
 
     def format_time2(self, obj):
-        return timezone.localtime(obj.time2).strftime('%Y-%m-%d %H:%M:%S') if obj.time2 else None
+        return self.format_time(obj.time2)
     format_time2.admin_order_field = 'time2'
     format_time2.short_description = 'Time 2'
 
     def format_time3(self, obj):
-        return timezone.localtime(obj.time3).strftime('%Y-%m-%d %H:%M:%S') if obj.time3 else None
+        return self.format_time(obj.time3)
     format_time3.admin_order_field = 'time3'
     format_time3.short_description = 'Time 3'
+
+    def format_time(self, datetime_obj):
+        """Formats a datetime object to display in the local timezone."""
+        if datetime_obj is None:
+            return 'Not Marked'  # or any other placeholder you prefer
+        if timezone.is_naive(datetime_obj):
+            # Assume the datetime is in UTC for making it timezone aware
+            datetime_obj = timezone.make_aware(datetime_obj, timezone.utc)
+        # Convert to local time in the Asia/Kolkata timezone
+        local_datetime = timezone.localtime(datetime_obj, timezone.get_default_timezone())
+        return local_datetime.strftime('%Y-%m-%d %H:%M:%S')
+
 
 admin.site.register(Attendance, AttendanceAdmin)
