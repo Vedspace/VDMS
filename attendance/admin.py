658815d13fb675228edf0b4493eca09ed28ffa08
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 00:41:51 +0530

    Update admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -5,32 +5,18 @@ from .models import Attendance
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('worker', 'date', 'format_time1', 'format_time2', 'format_time3')
 
-    def make_aware(self, datetime_obj):
-        if datetime_obj is not None and timezone.is_naive(datetime_obj):
-            return timezone.make_aware(datetime_obj, timezone.get_default_timezone())
-        return datetime_obj
-
     def format_time1(self, obj):
-        if obj.time1:
-            local_time = self.make_aware(obj.time1)
-            return local_time.strftime('%Y-%m-%d %H:%M:%S')
-        return None
+        return timezone.localtime(obj.time1).strftime('%Y-%m-%d %H:%M:%S') if obj.time1 else None
     format_time1.admin_order_field = 'time1'
     format_time1.short_description = 'Time 1'
 
     def format_time2(self, obj):
-        if obj.time2:
-            local_time = self.make_aware(obj.time2)
-            return local_time.strftime('%Y-%m-%d %H:%M:%S')
-        return None
+        return timezone.localtime(obj.time2).strftime('%Y-%m-%d %H:%M:%S') if obj.time2 else None
     format_time2.admin_order_field = 'time2'
     format_time2.short_description = 'Time 2'
 
     def format_time3(self, obj):
-        if obj.time3:
-            local_time = self.make_aware(obj.time3)
-            return local_time.strftime('%Y-%m-%d %H:%M:%S')
-        return None
+        return timezone.localtime(obj.time3).strftime('%Y-%m-%d %H:%M:%S') if obj.time3 else None
     format_time3.admin_order_field = 'time3'
     format_time3.short_description = 'Time 3'
 
