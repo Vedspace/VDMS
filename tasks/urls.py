b157f5e9ff7a1b1760a780cd074acafafae4534e
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-04-28 11:12:03 +0530

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,24 @@
+# Assuming you have this setup already
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, CategoryViewSet, UserAssignedTasksView, TokenLoginView, mark_task_as_complete
+from . import views
+from .views import mark_task_as_complete
+
+
+from .views import TaskViewSet,LoginView,TokenLoginView,CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
-router.register(r'categories', CategoryViewSet, basename='category')  # Registering the Category viewset
+router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('my-tasks/', UserAssignedTasksView.as_view(), name='my-tasks'),
+    
+    path('my-tasks/', views.UserAssignedTasksView.as_view(), name='my-tasks'),
     path('api/token-login/', TokenLoginView.as_view(), name='token_login'),
     path('api/tasks/<int:task_id>/complete/', mark_task_as_complete, name='mark-task-complete'),
 ]
+
+
+
+
