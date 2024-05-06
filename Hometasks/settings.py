e97ee006bdc7d1dbed386dd08e4a65f39ad273b1
Vedspace <105591834+Vedspace@users.noreply.github.com>
2024-05-06 17:29:13 +0530

    Update settings.py
    Removed React Native Access

--- Hometasks/settings.py
+++ Hometasks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2tn_r8!d9j4chdxuionnx6fkectp3cxu)5fo64_26qihzu8%8@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2','127.0.0.1','*']
+ALLOWED_HOSTS = ['127.0.0.1','*']
 
 
 # Application definition
